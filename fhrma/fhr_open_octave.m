000fb8d73022e51d3212592d2c208b6f6bcafeae
amyheather <a.heather2@exeter.ac.uk>
2023-12-12 16:09:36 +0000

    Convert .fhr to .csv in Octave using FHRMA's fhropen function

---
+++ fhrma/fhr_open_octave.m
@@ -0,0 +1,29 @@
+clear;
+
+% Set filename
+filename='test02';
+
+% Open file
+f=fopen([filename, '.fhr'], 'r');
+
+% Load timestamp for beginning of recording
+timestamp=fread(f,1,'uint32');
+
+% Load FHR data - first corresponds to first sensor, and
+% second corresponds to second sensor (which they don't use)
+data=fread(f,[3,10000000],'uint16');
+FHR1=data(1,:)/4;
+FHR2=data(2,:)/4;
+
+fseek(f,4,'bof');
+
+% TOCO signal
+data=fread(f,[6,10000000],'uint8');
+TOCO=data(5,:)/2;
+
+MHR=zeros(size(FHR1));
+infos=[];
+fclose(f);
+
+% Save FHR1 to a file for use in python
+csvwrite([filename, '.csv'], transpose(FHR1));
